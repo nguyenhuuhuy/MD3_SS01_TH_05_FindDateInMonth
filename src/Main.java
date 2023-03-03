9255743c76b026a410f4b4d15d6cf0b661a28848
nguyenhuuhuy <huy.cb96@gmail.com>
2023-03-03 07:51:10 +0700

    update!

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int month = scanner.nextInt();
         switch (month) {
             case 2:
-                System.out.println("the month"+month+"has 28 or 29 days");
+                System.out.println("the month"+month+"has 28 or 29 days!");
                 break;
             case 1:
             case 3:
@@ -16,16 +16,17 @@ public class Main {
             case 8:
             case 10:
             case 12:
-                System.out.println("the month" + month + "has 31 days");
+                System.out.println("the month" + month + "has 31 days!");
                 break;
             case 4:
             case 6:
             case 9:
             case 11:
-                System.out.println("the month"+ month+ "has 30 days");
+                System.out.println("the month"+ month+ "has 30 days!");
                 break;
             default:
-                System.out.println("this month doesn't exist!");
+                System.out.println("this month doesn't exist!!");
         }
+
     }
 }
